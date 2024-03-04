3a805880395150cc534aab1a538d22feb17dfd38
TamNhien <canhthien91@gmail.com>
2024-03-04 20:55:47 +0700

    Add project files.

---
+++ LTCB_BoiSoNVoiCacSo1Den20.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Nhap so nguyen n: ";
+    cin >> n;
+
+    cout << "Cac boi so cua " << n << " voi cac so tu 1 den 20 la:" << endl;
+    for (int i = 1; i <= 20; ++i) {
+        cout << n * i << " ";
+    }
+
+    return 0;
+}
